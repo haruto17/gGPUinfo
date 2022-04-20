8333536b785639967478a7556d58c64b581828ce
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-04-20 17:48:43 +0900

    Can show some GPU infomation.

--- gGPUinfo/main.cpp
+++ gGPUinfo/main.cpp
@@ -2,11 +2,48 @@
 
 int main()
 {
+	//Get timestamp
+	system("nvidia-smi --query-gpu=timestamp --format=csv");
+
+	std::cout << std::endl;
+
 	//Get GPU name
-	system("nvidia-smi --list-gpus");
+	system("nvidia-smi --query-gpu=gpu_name --format=csv");
+
+	std::cout << std::endl;
+
+	//Get driver_version
+	system("nvidia-smi --query-gpu=driver_version --format=csv");
+
+	std::cout << std::endl;
 
 	//Get GPU temperature
-	system("nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader");
+	system("nvidia-smi --query-gpu=temperature.gpu --format=csv");
+
+	std::cout << std::endl;
+
+	//Get GPU utilization
+	system("nvidia-smi --query-gpu=utilization.gpu --format=csv");
+
+	std::cout << std::endl;
+
+	//Get GPU memory utilization
+	system("nvidia-smi --query-gpu=utilization.memory --format=csv");
+
+	std::cout << std::endl;
+
+	//Get total GPU memory
+	system("nvidia-smi --query-gpu=memory.total --format=csv");
+
+	std::cout << std::endl;
+
+	//Get free GPU memory
+	system("nvidia-smi --query-gpu=memory.free --format=csv");
+
+	std::cout << std::endl;
+
+	//Get used GPU memory
+	system("nvidia-smi --query-gpu=memory.used --format=csv");
 
 	return 0;
 }
